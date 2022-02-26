6dba54b238cc56e7c0857115c22a3ff249285ee3
Freddy Cansick <freddycansick@gmail.com>
2022-02-26 09:49:22 +0000

    Initial commit

---
+++ core/src/com/crypter/game/Main.java
@@ -0,0 +1,24 @@
+package com.crypter.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Main extends Game {
+	
+	
+	@Override
+	public void create () {
+		
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+
+	}
+	
+	@Override
+	public void dispose () {
+		
+	}
+}
---
+++ desktop/src/com/crypter/game/DesktopLauncher.java
@@ -0,0 +1,26 @@
+package com.crypter.game;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("CRYPTER");
+
+//		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+		config.setWindowedMode(1280, 720);
+		config.setResizable(false);
+//		config.samples = 3; // anti aliasing
+//		config.forceExit = false;
+		
+//		config.addIcon("icons/icon_128.png", FileType.Internal);
+//		config.addIcon("icons/icon_32.png", FileType.Internal);
+//		config.addIcon("icons/icon_16.png", FileType.Internal);
+		
+		new Lwjgl3Application(new Main(), config);
+	}
+}
