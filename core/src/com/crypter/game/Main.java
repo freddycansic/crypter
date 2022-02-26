846ffb0ee69d0b3232c0a0a5a5a95744151bfac3
Freddy Cansick <freddycansick@gmail.com>
2022-02-26 10:44:18 +0000

    Boilerplate

--- core/src/com/crypter/game/Main.java
+++ core/src/com/crypter/game/Main.java
@@ -1,24 +1,42 @@
 package com.crypter.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.crypter.game.scenes.Level1;
+import com.crypter.game.scenes.Scene;
+import com.crypter.game.util.Resources;
 
 public class Main extends Game {
 	
+	private static Main instance;
+	private Scene currentScene;
+	private Resources rs;
 	
 	@Override
 	public void create () {
+		instance = this;
+		rs = new Resources();
 		
+		Main.setScene(new Level1(rs));
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
 
+		currentScene.act();
+		currentScene.render();
+		
 	}
 	
 	@Override
 	public void dispose () {
-		
+		rs.dispose();
+	}
+
+	public static void setScene(Scene scene) {
+		instance.currentScene = scene;
+		Gdx.input.setInputProcessor(scene);
 	}
 }
