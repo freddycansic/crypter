88cafebe0291cf3b5e8312463e041407c8c6d5e5
Freddy Cansick <freddycansick@gmail.com>
2022-03-12 13:54:10 +0000

    New tilemap

--- assets/tilemaps/tileset.tsx
+++ assets/tilemaps/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="tileset" tilewidth="32" tileheight="32" tilecount="16" columns="8">
- <image source="tileset.png" width="256" height="64"/>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="21" columns="3">
+ <image source="tileset_spritesheet.png" width="128" height="256"/>
 </tileset>
